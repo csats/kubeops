2bedfc9f2ea9b1578a13e8bbf196247049647df2
Eli Mallon <eli.mallon@csats.com>
2016-01-24 01:21:23 +0000

    Add coreos-kubernetes

---
+++ Godeps/_workspace/src/github.com/csats/coreos-kubernetes/multi-node/aws/pkg/cluster/cluster.go
@@ -0,0 +1,268 @@
+package cluster
+
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"text/tabwriter"
+
+	"github.com/csats/kubeops/Godeps/_workspace/src/github.com/aws/aws-sdk-go/aws"
+	"github.com/csats/kubeops/Godeps/_workspace/src/github.com/aws/aws-sdk-go/service/cloudformation"
+	"github.com/csats/kubeops/Godeps/_workspace/src/github.com/aws/aws-sdk-go/service/ec2"
+)
+
+type ClusterInfo struct {
+	Name         string
+	ControllerIP string
+}
+
+func (c *ClusterInfo) String() string {
+	buf := new(bytes.Buffer)
+	w := new(tabwriter.Writer)
+	w.Init(buf, 0, 8, 0, '\t', 0)
+
+	fmt.Fprintf(w, "Cluster Name:\t%s\n", c.Name)
+	fmt.Fprintf(w, "Controller IP:\t%s\n", c.ControllerIP)
+
+	w.Flush()
+	return buf.String()
+}
+
+type TLSConfig struct {
+	CACertFile string
+	CACert     []byte
+
+	APIServerCertFile string
+	APIServerCert     []byte
+	APIServerKeyFile  string
+	APIServerKey      []byte
+
+	WorkerCertFile string
+	WorkerCert     []byte
+	WorkerKeyFile  string
+	WorkerKey      []byte
+
+	AdminCertFile string
+	AdminCert     []byte
+	AdminKeyFile  string
+	AdminKey      []byte
+}
+
+func New(cfg *Config, awsConfig *aws.Config) *Cluster {
+	return &Cluster{
+		cfg: cfg,
+		aws: awsConfig,
+	}
+}
+
+type Cluster struct {
+	cfg *Config
+	aws *aws.Config
+}
+
+func (c *Cluster) stackName() string {
+	return c.cfg.ClusterName
+}
+
+func (c *Cluster) Create(tlsConfig *TLSConfig) error {
+	parameters := []*cloudformation.Parameter{
+		{
+			ParameterKey:     aws.String(parClusterName),
+			ParameterValue:   aws.String(c.stackName()),
+			UsePreviousValue: aws.Bool(true),
+		},
+		{
+			ParameterKey:     aws.String(parNameKeyName),
+			ParameterValue:   aws.String(c.cfg.KeyName),
+			UsePreviousValue: aws.Bool(true),
+		},
+		{
+			ParameterKey:     aws.String(parArtifactURL),
+			ParameterValue:   aws.String(c.cfg.ArtifactURL),
+			UsePreviousValue: aws.Bool(true),
+		},
+		{
+			ParameterKey:     aws.String(parCACert),
+			ParameterValue:   aws.String(base64.StdEncoding.EncodeToString(tlsConfig.CACert)),
+			UsePreviousValue: aws.Bool(true),
+		},
+		{
+			ParameterKey:     aws.String(parAPIServerCert),
+			ParameterValue:   aws.String(base64.StdEncoding.EncodeToString(tlsConfig.APIServerCert)),
+			UsePreviousValue: aws.Bool(true),
+		},
+		{
+			ParameterKey:     aws.String(parAPIServerKey),
+			ParameterValue:   aws.String(base64.StdEncoding.EncodeToString(tlsConfig.APIServerKey)),
+			UsePreviousValue: aws.Bool(true),
+		},
+		{
+			ParameterKey:     aws.String(parWorkerCert),
+			ParameterValue:   aws.String(base64.StdEncoding.EncodeToString(tlsConfig.WorkerCert)),
+			UsePreviousValue: aws.Bool(true),
+		},
+		{
+			ParameterKey:     aws.String(parWorkerKey),
+			ParameterValue:   aws.String(base64.StdEncoding.EncodeToString(tlsConfig.WorkerKey)),
+			UsePreviousValue: aws.Bool(true),
+		},
+	}
+
+	if c.cfg.ReleaseChannel != "" {
+		parameters = append(parameters, &cloudformation.Parameter{
+			ParameterKey:     aws.String(parNameReleaseChannel),
+			ParameterValue:   aws.String(c.cfg.ReleaseChannel),
+			UsePreviousValue: aws.Bool(true),
+		})
+	}
+
+	if c.cfg.ControllerInstanceType != "" {
+		parameters = append(parameters, &cloudformation.Parameter{
+			ParameterKey:     aws.String(parNameControllerInstanceType),
+			ParameterValue:   aws.String(c.cfg.ControllerInstanceType),
+			UsePreviousValue: aws.Bool(true),
+		})
+	}
+
+	if c.cfg.ControllerRootVolumeSize > 0 {
+		parameters = append(parameters, &cloudformation.Parameter{
+			ParameterKey:     aws.String(parNameControllerRootVolumeSize),
+			ParameterValue:   aws.String(fmt.Sprintf("%d", c.cfg.ControllerRootVolumeSize)),
+			UsePreviousValue: aws.Bool(true),
+		})
+	}
+
+	if c.cfg.ControllerTenancy != "" {
+		parameters = append(parameters, &cloudformation.Parameter{
+			ParameterKey:     aws.String(parControllerTenancy),
+			ParameterValue:   aws.String(c.cfg.ControllerTenancy),
+			UsePreviousValue: aws.Bool(true),
+		})
+	}
+
+	if c.cfg.WorkerCount > 0 {
+		parameters = append(parameters, &cloudformation.Parameter{
+			ParameterKey:     aws.String(parWorkerCount),
+			ParameterValue:   aws.String(fmt.Sprintf("%d", c.cfg.WorkerCount)),
+			UsePreviousValue: aws.Bool(true),
+		})
+	}
+
+	if c.cfg.WorkerInstanceType != "" {
+		parameters = append(parameters, &cloudformation.Parameter{
+			ParameterKey:     aws.String(parNameWorkerInstanceType),
+			ParameterValue:   aws.String(c.cfg.WorkerInstanceType),
+			UsePreviousValue: aws.Bool(true),
+		})
+	}
+
+	if c.cfg.WorkerRootVolumeSize > 0 {
+		parameters = append(parameters, &cloudformation.Parameter{
+			ParameterKey:     aws.String(parNameWorkerRootVolumeSize),
+			ParameterValue:   aws.String(fmt.Sprintf("%d", c.cfg.WorkerRootVolumeSize)),
+			UsePreviousValue: aws.Bool(true),
+		})
+	}
+
+	if c.cfg.WorkerSpotPrice != "" {
+		parameters = append(parameters, &cloudformation.Parameter{
+			ParameterKey:     aws.String(parWorkerSpotPrice),
+			ParameterValue:   aws.String(c.cfg.WorkerSpotPrice),
+			UsePreviousValue: aws.Bool(true),
+		})
+	}
+
+	if c.cfg.WorkerTenancy != "" {
+		parameters = append(parameters, &cloudformation.Parameter{
+			ParameterKey:     aws.String(parWorkerTenancy),
+			ParameterValue:   aws.String(c.cfg.WorkerTenancy),
+			UsePreviousValue: aws.Bool(true),
+		})
+	}
+
+	if c.cfg.AvailabilityZone != "" {
+		parameters = append(parameters, &cloudformation.Parameter{
+			ParameterKey:     aws.String(parAvailabilityZone),
+			ParameterValue:   aws.String(c.cfg.AvailabilityZone),
+			UsePreviousValue: aws.Bool(true),
+		})
+	}
+
+	if c.cfg.VPCCIDR != "" {
+		parameters = append(parameters, &cloudformation.Parameter{
+			ParameterKey:     aws.String(parVPCCIDR),
+			ParameterValue:   aws.String(c.cfg.VPCCIDR),
+			UsePreviousValue: aws.Bool(true),
+		})
+	}
+
+	if c.cfg.InstanceCIDR != "" {
+		parameters = append(parameters, &cloudformation.Parameter{
+			ParameterKey:     aws.String(parInstanceCIDR),
+			ParameterValue:   aws.String(c.cfg.InstanceCIDR),
+			UsePreviousValue: aws.Bool(true),
+		})
+	}
+
+	if c.cfg.ControllerIP != "" {
+		parameters = append(parameters, &cloudformation.Parameter{
+			ParameterKey:     aws.String(parControllerIP),
+			ParameterValue:   aws.String(c.cfg.ControllerIP),
+			UsePreviousValue: aws.Bool(true),
+		})
+	}
+
+	if c.cfg.PodCIDR != "" {
+		parameters = append(parameters, &cloudformation.Parameter{
+			ParameterKey:     aws.String(parPodCIDR),
+			ParameterValue:   aws.String(c.cfg.PodCIDR),
+			UsePreviousValue: aws.Bool(true),
+		})
+	}
+
+	if c.cfg.ServiceCIDR != "" {
+		parameters = append(parameters, &cloudformation.Parameter{
+			ParameterKey:     aws.String(parServiceCIDR),
+			ParameterValue:   aws.String(c.cfg.ServiceCIDR),
+			UsePreviousValue: aws.Bool(true),
+		})
+	}
+
+	if c.cfg.KubernetesServiceIP != "" {
+		parameters = append(parameters, &cloudformation.Parameter{
+			ParameterKey:     aws.String(parKubernetesServiceIP),
+			ParameterValue:   aws.String(c.cfg.KubernetesServiceIP),
+			UsePreviousValue: aws.Bool(true),
+		})
+	}
+
+	if c.cfg.DNSServiceIP != "" {
+		parameters = append(parameters, &cloudformation.Parameter{
+			ParameterKey:     aws.String(parDNSServiceIP),
+			ParameterValue:   aws.String(c.cfg.DNSServiceIP),
+			UsePreviousValue: aws.Bool(true),
+		})
+	}
+
+	tmplURL := fmt.Sprintf("%s/template.json", c.cfg.ArtifactURL)
+	return createStackAndWait(cloudformation.New(c.aws), c.stackName(), tmplURL, parameters)
+}
+
+func (c *Cluster) Info() (*ClusterInfo, error) {
+	resources, err := getStackResources(cloudformation.New(c.aws), c.stackName())
+	if err != nil {
+		return nil, err
+	}
+
+	info, err := mapStackResourcesToClusterInfo(ec2.New(c.aws), resources)
+	if err != nil {
+		return nil, err
+	}
+
+	info.Name = c.cfg.ClusterName
+	return info, nil
+}
+
+func (c *Cluster) Destroy() error {
+	return destroyStack(cloudformation.New(c.aws), c.stackName())
+}
---
+++ cmd/clusterUp.go
@@ -0,0 +1,52 @@
+// Copyright © 2016 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/csats/coreos-kubernetes/multi-node/aws/pkg/cluster"
+)
+
+// clusterUpCmd represents the clusterUp command
+var clusterUpCmd = &cobra.Command{
+	Use:   "cluster-up",
+	Short: "Create a Kubernetes cluster",
+	Long:  `Given a kubeops YAML file, create a CoreOS AWS Kubernetes cluster.`,
+	Run:   runClusterUp,
+}
+
+func runClusterUp(cmd *cobra.Command, args []string) {
+	// TODO: Work your own magic here
+	fmt.Println("clusterUp called")
+	fmt.Printf("%v", cluster.New)
+}
+
+func init() {
+	RootCmd.AddCommand(clusterUpCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// clusterUpCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// clusterUpCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+}
